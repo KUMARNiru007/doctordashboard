efba173ab65f05b02f00dd190dcbed06576ea404
Kumar Nirupam <kumar.nirupam24@gmail.com>
2025-06-01 13:49:33 +0530

    setup machine in local host

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DoctorDashboard from './DoctorDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <DoctorDashboard />
+    </div>
+  );
+}
+
+export default App;
